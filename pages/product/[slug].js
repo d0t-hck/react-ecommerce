87f90f009d3315c7dcdd291fe49739fddf437ff2
hck <aza.shaikov@gmail.com>
2022-06-21 20:36:05 +0600

    product detail

---
+++ pages/product/[slug].js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+import { client, urlFor } from '../../lib/client';
+import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
+
+const ProductDetails = ({ product, products }) => {
+    const { image, title, details, price, categories } = product;
+    const [index, setIndex] = useState(0);
+    const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+    const handleBuyNow = () => {
+        onAdd(product, qty);
+
+        setShowCart(true);
+    }
+
+    return (
+        <div>
+            <div className='product-detail-container'>
+                <div>
+                    <div className='image-container'>
+                        <img src={urlFor(image && image[index])} className='product-detail-image' />
+                    </div>
+                    <div className="small-images-container">
+                        {image?.map((item, i) => (
+                            <img
+                                key={i}
+                                src={urlFor(item)}
+                                className={i === index ? 'small-image selected-image' : 'small-image'}
+                                onMouseEnter={() => setIndex(i)}
+                            />
+                        ))}
+                    </div>
+                </div>
+
+                <div className='product-detail-desc'>
+                    <h1>{title}</h1>
+                    <h4>Описание: </h4>
+                    <p>{details}</p>
+                    <p className='price'>{price} &#x20B8;</p>
+                    <div className='quantity'>
+                        <h3>Количество:</h3>
+                        <p className='quantity-desc'>
+                            <span className='minus' onClick={decQty}><AiOutlineMinus /></span>
+                            <span className='num' onClick=''>{qty}</span>
+                            <span className='plus' onClick={incQty}><AiOutlinePlus /></span>
+                        </p>
+                    </div>
+                    <div className='buttons'>
+                        <button type='button' className='add-to-cart' onClick={() => onAdd(product, qty)}>
+                            В корзину
+                        </button>
+                        <button type='button' className='buy-now' onClick={handleBuyNow}>
+                            Купить
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+            <div className="maylike-products-wrapper">
+                <div className="marquee">
+                    <div className="maylike-products-container track">
+                        {products.map((item) => (
+                            <Product key={item._id} product={item} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export const getStaticPaths = async () => {
+    const query = `*[_type == "product"] {
+        slug {
+            current
+        }
+    }`;
+
+    const products = await client.fetch(query);
+
+    const paths = products.map((product) => ({
+        params: {
+            slug: product.slug.current
+        }
+    }));
+
+    return {
+        paths,
+        fallback: 'blocking'
+    }
+}
+
+export const getStaticProps = async ({ params: { slug } }) => {
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+    const productsQuery = '*[_type == "product"]'
+
+    const product = await client.fetch(query);
+    const products = await client.fetch(productsQuery);
+
+    console.log(product);
+
+    return {
+        props: { products, product }
+    }
+}
+
+export default ProductDetails
